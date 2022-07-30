8507e08c45baa82ffe68a416998dd8e483bd1d71
gitdoallan <mailtoallan@icloud.com>
2022-07-30 18:10:56 -0300

    feat: implementing header translation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,25 @@
 import { HeaderLink } from '@components/HeaderLink';
 import { FC } from 'react';
 import { Logo } from '../Logo';
+import { namespaces } from 'i18n/i18n.constants';
+import { useTranslation } from 'react-i18next';
 
 import * as S from './styles';
 
-export const Header: FC = () => (
-  <S.HeaderContainer data-testid="header">
-    <S.HeaderContent>
-      <Logo />
-      <S.HeaderLinksContainer>
-        <HeaderLink to="a">Clube</HeaderLink>
-        <HeaderLink to="b">Loja</HeaderLink>
-        <HeaderLink to="c">Produtores</HeaderLink>
-        <HeaderLink to="d">Ofertas</HeaderLink>
-        <HeaderLink to="e">Eventos</HeaderLink>
-      </S.HeaderLinksContainer>
-    </S.HeaderContent>
-  </S.HeaderContainer>
-);
+export const Header: FC = () => {
+  const { t } = useTranslation(namespaces.header);
+  return (
+    <S.HeaderContainer data-testid="header">
+      <S.HeaderContent>
+        <Logo />
+        <S.HeaderLinksContainer>
+          <HeaderLink to="a">{t('links.club')}</HeaderLink>
+          <HeaderLink to="b">{t('links.store')}</HeaderLink>
+          <HeaderLink to="c">{t('links.producers')}</HeaderLink>
+          <HeaderLink to="d">{t('links.specialOffers')}</HeaderLink>
+          <HeaderLink to="e">{t('links.events')}</HeaderLink>
+        </S.HeaderLinksContainer>
+      </S.HeaderContent>
+    </S.HeaderContainer>
+  );
+};
--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,19 +1,9 @@
-import i18n, { Resource } from 'i18next';
+import i18n from 'i18next';
 import LanguageDetector, {
   DetectorOptions,
 } from 'i18next-browser-languagedetector';
 import XHR from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
-
-import commonEn from './locales/en/common.json';
-import commonEs from './locales/es/common.json';
-import commonPt from './locales/pt/common.json';
-
-const resources: Resource = {
-  pt: { common: commonPt },
-  en: { common: commonEn },
-  es: { common: commonEs },
-};
 
 const options: DetectorOptions = {
   order: ['path', 'navigator'],
@@ -25,8 +15,10 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    backend: {
+      loadPath: './src/i18n/locales/{{lng}}/{{ns}}.json', // Specify where backend will find translation files.
+    },
     detection: options,
-    resources,
     ns: ['common'],
     defaultNS: 'common',
     fallbackLng: 'pt',
