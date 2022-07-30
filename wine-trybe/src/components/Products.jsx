7fcefff1aa65b857abe14496fd7ec9ff06e11082
RegislaineRegis <nanafidelis@gmail.com>
2022-07-30 15:14:52 -0300

    feat:iniciando cardProduct

--- wine-trybe/src/components/Products.jsx
+++ wine-trybe/src/components/Products.jsx
@@ -4,6 +4,7 @@ import { apiProducts } from "../services/api";
 export default function Products() {
   const [products, setProducts] = useState();
   console.log(products)
+  const maxLength = 6;
 
   useEffect(() => {
     apiProducts
@@ -14,9 +15,20 @@ export default function Products() {
       });
   }, []);
 
-  return (
-    <div className="App">
-    </div>
-  );
+  return products.map((prod, index ) => {
+    if (index < maxLength) {
+      return(
+        <div className="App"
+        key={ index }>
+          <img
+            src={ products.image }
+            alt={ prod.name }
+            width="100"
+            height="100"
+          />
+        </div>
+      );
+    } return null;
+  })
 }
 
