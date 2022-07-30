dba91067398ca0c53f29c0772e5cadeda5caa4e6
Thiago <bodnarthiago@gmail.com>
2022-07-30 13:28:33 -0300

    ops: adiciona configurações ao .eslitrc

--- landing-page-group-3/src/components/Card.js
+++ landing-page-group-3/src/components/Card.js
@@ -1,10 +1,8 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 
-const Card = ({ src, alt }) => (
-    //   <Link to={  }>
-    <img src={src} alt={alt} />
-    //   </Link>
-);
+function Card({ src, alt }) {
+  return <img src={src} alt={alt} />;
+}
 
 export default Card;
--- landing-page-group-3/src/components/Header.js
+++ landing-page-group-3/src/components/Header.js
@@ -7,51 +7,50 @@ import React, { useContext } from 'react';
 // import BarraDeBusca from './BarraDeBusca';
 
 export default function Header() {
-    const { setSearchBar, searchBar } = useContext(AppContext);
-    const notRender = ['/profile',
-        '/explore',
-        '/explore/drinks',
-        '/explore/foods',
-        '/details',
-        '/explore/drinks/ingredients',
-        '/explore/foods/ingredients',
-        '/done-recipes',
-        '/favorite-recipes',
+  const { setSearchBar, searchBar } = useContext(AppContext);
+  const notRender = ['/profile',
+    '/explore',
+    '/explore/drinks',
+    '/explore/foods',
+    '/details',
+    '/explore/drinks/ingredients',
+    '/explore/foods/ingredients',
+    '/done-recipes',
+    '/favorite-recipes',
 
-    ];
+  ];
 
-    const match = useRouteMatch();
+  const match = useRouteMatch();
 
-    return (
-        <div className="main-header">
-            <header
-                data-testid="page-title"
-            >
-                <Link to="/profile">
-                    <button
-                        type="button"
-
-                    >
-                        <img src={profileIcon} alt="profile icon" data-testid="profile-top-btn" />
-                    </button>
-                </Link>
-                {
+  return (
+    <div className="main-header">
+      <header
+        data-testid="page-title"
+      >
+        <Link to="/profile">
+          <button
+            type="button"
+          >
+            <img src={profileIcon} alt="profile icon" data-testid="profile-top-btn" />
+          </button>
+        </Link>
+        {
                     !notRender.includes(match.path)
                     && (
-                        <button
-                            type="button"
-                            onClick={() => setSearchBar(!searchBar)}
-                        >
-                            <img src={searchIcon} alt=" search icon" data-testid="search-top-btn" />
-                        </button>
+                    <button
+                      type="button"
+                      onClick={() => setSearchBar(!searchBar)}
+                    >
+                      <img src={searchIcon} alt=" search icon" data-testid="search-top-btn" />
+                    </button>
                     )
                 }
-            </header>
-            {
+      </header>
+      {
                 searchBar && (
-                    <BarraDeBusca />
+                <BarraDeBusca />
                 )
             }
-        </div>
-    );
+    </div>
+  );
 }
