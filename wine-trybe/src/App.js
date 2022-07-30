3c44fa820e5264e6121b665d33e8b8d050a3e750
vnpuga <vnpuga@gmail.com>
2022-07-30 18:23:46 -0300

    feat: cria componente header e estilização

--- wine-trybe/src/App.js
+++ wine-trybe/src/App.js
@@ -1,10 +1,12 @@
 import Plans from "./components/Plans";
 import Products from "./components/Products";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <Products />
         <Plans />
       </header>
