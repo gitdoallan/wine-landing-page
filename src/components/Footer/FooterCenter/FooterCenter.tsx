1c3f7445fd6fcaa47ebd30c8a86c211ec89e4c1a
EmersonAlves <emersondasilva.alves99@gmail.com>
2022-07-30 20:42:26 -0300

    fix: about component

--- src/components/Footer/FooterCenter/FooterCenter.tsx
+++ src/components/Footer/FooterCenter/FooterCenter.tsx
@@ -1,4 +1,4 @@
-import * as S from './styles';
+import { Container } from './styles';
 
 export const FooterCenter = () => {
   const networks = [
@@ -24,7 +24,7 @@ export const FooterCenter = () => {
     },
   ];
   return (
-    <S.Container>
+    <Container>
       <h3>Alguma dúvida? A gente ajuda você!</h3>
       <h4>Horário de atendimento:</h4>
       <p>De segunda a sexta-feira de 09h as 18h. Exceto feriados.</p>
@@ -39,6 +39,6 @@ export const FooterCenter = () => {
           </a>
         ))}
       </div>
-    </S.Container>
+    </Container>
   );
 };
