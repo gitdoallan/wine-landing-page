ec187ca32028390c09f52f29bfedd443c320d028
Matheus <stvgamer74@gmail.com>
2022-07-30 20:26:12 -0300

    fix: set variable S in imports styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { Container } from './styles';
+import * as S from './styles';
 import { FooterRight } from './FooterRight';
 import { FooterCenter } from './FooterCenter';
 import { FooterLeft } from './FooterLeft';
 
 export const Footer: React.FC = () => (
-  <Container>
+  <S.Container>
     <FooterRight />
     <FooterCenter />
     <FooterLeft />
-  </Container>
+  </S.Container>
 );
--- src/utils/checkDarkMode.ts
+++ src/utils/checkDarkMode.ts
@@ -2,5 +2,5 @@ export const isDarkModeActive = window.matchMedia(
   '(prefers-color-scheme: dark)',
 ).matches;
 
-if (isDarkModeActive) console.log('DARK MODE');
-else console.log('NORMAL MODE');
+if (isDarkModeActive) console.log('DARK MODE'); //TODO
+else console.log('NORMAL MODE'); //TODO
