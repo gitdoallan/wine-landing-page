698738f6623ef9be653116cb0eceb1edb128808b
Mauro <MauroMarchesan88@gmail.com>
2022-07-30 12:40:22 -0300

    Eslint funcionando

---
+++ landing-page-group-3/src/components/Card.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+const Card = ({ src, alt }) => (
+    //   <Link to={  }>
+    <img src={src} alt={alt} />
+    //   </Link>
+);
+
+export default Card;
