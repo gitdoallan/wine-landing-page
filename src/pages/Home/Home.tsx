f48eb8504f2b04aded1ca6c89b3512d16670b9a6
EmersonAlves <emersondasilva.alves99@gmail.com>
2022-07-30 23:45:00 -0300

    feat: update header styles and moving links to Menu componete

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import { Footer } from '../../components/Footer';
 import React from 'react';
 
 import * as S from './styles';
-import { About } from '@components/About';
+import { About } from '../../components/About';
 
 export const Home: React.FC = () => (
   <S.HomeContainer>
