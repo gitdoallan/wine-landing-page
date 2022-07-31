4a55e054b5f376574b7b9eac2d64432bcb5e3caa
Matheus <stvgamer74@gmail.com>
2022-07-31 02:21:24 -0300

    chore: add library phosphor-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Home } from './pages/Home';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
+import { IconContext } from "phosphor-react";
 import store from './redux/store';
 import { GlobalStyles } from './styles/global/GlobalStyles';
 import { defaultTheme } from './styles/themes';
@@ -8,8 +9,17 @@ import { defaultTheme } from './styles/themes';
 const App = () => (
   <Provider store={store}>
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <Home />
+      <IconContext.Provider
+        value={{
+          color: "limegreen",
+          size: 32,
+          weight: "bold",
+          mirrored: false,
+        }}
+      >
+        <GlobalStyles />
+        <Home />
+      </IconContext.Provider>
     </ThemeProvider>
   </Provider>
 );
