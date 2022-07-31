73014bea0e404d97006bc9c4fd96da43e98179c8
gitdoallan <mailtoallan@icloud.com>
2022-07-30 22:14:27 -0300

    feat: footerRight texts translated

--- src/components/Footer/FooterRight/FooterRight.tsx
+++ src/components/Footer/FooterRight/FooterRight.tsx
@@ -1,24 +1,26 @@
 import { Container } from './styles';
+import { namespaces } from 'i18n/i18n.constants';
+import { useTranslation } from 'react-i18next';
 
-export const FooterRight = () => (
-  <Container>
-    <div>
-      <img
-        src="https://img.wine.com.br/fenix/image/_big_bang/logos/logo_w.svg"
-        alt="Logo Wine"
-      />
+export const FooterRight = () => {
+  const { t } = useTranslation(namespaces.footer);
+  return (
+    <Container>
       <div>
-        <h3>W2W E-Commerce de Vinhos S.A</h3>
-        <p>CNPJ: 09.813.204/0002-05</p>
+        <img
+          src="https://img.wine.com.br/fenix/image/_big_bang/logos/logo_w.svg"
+          alt="Logo Wine"
+        />
+        <div>
+          <h3>W2W E-Commerce de Vinhos S.A</h3>
+          <p>CNPJ: 09.813.204/0002-05</p>
+        </div>
       </div>
-    </div>
-    <address>
-      <p>Rua Comendador Alcides Simão Helou, 1565</p>
-      <p>Serra - ES - CEP 29168-090</p>
-    </address>
-    <p>
-      SE BEBER, NÃO DIRIJA. APRECIE COM MODERAÇÃO. A VENDA DE BEBIDAS ALCOÓLICAS
-      É PROIBIDA PARA MENORES DE 18 ANOS.
-    </p>
-  </Container>
-);
+      <address>
+        <p>Rua Comendador Alcides Simão Helou, 1565</p>
+        <p>Serra - ES - CEP 29168-090</p>
+      </address>
+      <p>{t('texts.alcoholWarning')}</p>
+    </Container>
+  );
+};
